4beafc48305049c2a3dbb78cffea335a1f5e8dd9
abctel <abctel@gmail.com>
2021-08-15 22:42:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,47 +19,13 @@ sed -i 's/192.168.1.1/192.168.100.200/g' package/base-files/files/bin/config_gen
 # 增加关机插件
 #git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/uci-app-poweroff
 
-# 增加Chinadns-NG插件
-#git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
-
-# 增加dockerman插件
-# rm -rf package/luci-lib-docker/
-# rm -rf package/lean/luci-app-dockerman/
-# git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker/
-git clone https://github.com/lisaac/luci-app-dockerman package/lean/luci-app-dockerman/
-#cp -rf package/lean/luci-app-dockerman/applications/luci-app-dockerman package/lean
-
-# 配置定制
-mkdir abctel
-git clone https://github.com/abctel/LEDE-Firmware-Config.git abctel/config
-
-mv -f abctel/config/passwall package/feeds/kenzo/luci-app-passwall/root/etc/config/passwall
-
-mkdir package/feeds/kenzo/luci-app-smartdns/root/etc/smartdns
-mv -f abctel/config/custom.conf package/feeds/kenzo/luci-app-smartdns/root/etc/smartdns/custom.conf
-mkdir package/feeds/kenzo/luci-app-smartdns/root/etc/config
-mv -f abctel/config/smartdns package/feeds/kenzo/luci-app-smartdns/root/etc/config/smartdns
-mv -f abctel/config/AdGuardHome.yaml package/feeds/kenzo/luci-app-adguardhome/root/etc/config/AdGuardHome.yaml
-mkdir package/feeds/kenzo/luci-app-adguardhome/root/usr/bin
-touch package/feeds/kenzo/luci-app-adguardhome/root/usr/bin/AdGuardHome
-mv -f abctel/config/update_core.sh package/feeds/kenzo/luci-app-adguardhome/root/usr/share/AdGuardHome/update_core.sh
-mv -f abctel/config/AdGuardHome_template.yaml package/feeds/kenzo/luci-app-adguardhome/root/usr/share/AdGuardHome/AdGuardHome_template.yaml
-
-# Docker
-#svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman package/luci-app-dockerman
-#git clone --depth=1 https://github.com/lisaac/luci-lib-docker
-#if [ -e feeds/packages/utils/docker-ce ];then
-#	sed -i '/dockerd/d' package/luci-app-dockerman/Makefile
-#	sed -i 's/+docker/+docker-ce/g' package/luci-app-dockerman/Makefile
-#fi
-
-# 增加一套主题
-#git clone https://github.com/siropboy/luci-theme-btmod package/lean/luci-theme-btmod
-
-# 删除老argon主题
-# rm -rf package/lean/luci-theme-argon
-# 拉取argon主题
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-# 说不得
-# git clone https://github.com/ITdesk01/jd_openwrt_script.git package/jd_openwrt_script
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/abctel/luci-app-smartdns
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/abctel/luci-app-adguardhome
+git clone https://github.com/xiaorouji/openwrt-passwall package/abctel/openwrt-passwall
+git clone https://github.com/abctel/luci-theme-edge package/abctel/luci-theme-edge
+git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/abctel/luci-app-chinadns-ng
+git clone https://github.com/abctel/abctel-package package/abctel/dnsmasq-china-list
+git clone https://github.com/lisaac/luci-app-dockerman package/abctel/luci-app-dockerman
+git clone https://github.com/ITdesk01/jd_openwrt_script.git package/abctel/jd_openwrt_script
+chmod 755 files/etc/init.d/smartdns
+chmod 755 files/usr/bin/AdGuardHome/AdGuardHome
